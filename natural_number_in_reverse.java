6968e022336cdc01a5d43849499ac1ac851b8a11
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-02-25 01:01:32 -0800

    Create natural_number_in_reverse.java

---
+++ natural_number_in_reverse.java
@@ -0,0 +1,12 @@
+//FIRST N NATURAL NUMBER IN REVERSE ORDER
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            System.out.println(i);    
+            }
+         }
+}
