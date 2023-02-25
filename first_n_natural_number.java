124d365cfec2cc470f2f2f7469995265176468b6
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-02-25 00:53:31 -0800

    Create first_n_natural_number.java

---
+++ first_n_natural_number.java
@@ -0,0 +1,13 @@
+//FIRST N ODD NUMBERS
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.println(i);
+        }
+        
+    }
+}
