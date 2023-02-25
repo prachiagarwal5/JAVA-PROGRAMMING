844e750c1f1103ebb3bffe0c0d4fc9e01d34c3a0
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-02-25 01:11:31 -0800

    Create number_112123_pattern.java

---
+++ number_112123_pattern.java
@@ -0,0 +1,15 @@
+//NUMBER PATTERN
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+             System.out.print(j);    
+            }
+            System.out.println(" ");    
+            }
+         }
+}
