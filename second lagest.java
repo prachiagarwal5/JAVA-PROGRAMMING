14cf2a971eb6d1d139ca786673f3cceb397c9512
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-06-22 21:30:27 -0700

    Create second lagest.java

---
+++ second lagest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int temp;
+		System.out.println("enter the size of array");
+		int n=sc.nextInt();
+		int [] num=new int[n];
+		int i;
+		System.out.println("enter the values in array");
+		for(i=0;i<n;i++){
+		    num[i]=sc.nextInt();
+		}
+	for(i=0;i<(n-1);i++){
+	    if(num[i+1]<num[i]){
+	        temp=num[i];
+	        num[i]=num[i+1];
+	        num[i+1]=temp;
+	    }
+	}
+	System.out.println("the second largest number is"+num[n-2]);
+	}
+}
