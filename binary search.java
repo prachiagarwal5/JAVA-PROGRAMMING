26331343c156b87521d58b48bfecff3ff5a1bd63
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-06-29 07:19:40 -0700

    Update binary search.java

--- binary search.java
+++ binary search.java
@@ -1,33 +1,52 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+  import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+     
 
-import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	int ar[]={11,24,37,49,50,67,72,81,97,104};
-	int target=81;
-	int left=0;
-	int right=ar.length-1;
-	int flag=0;
-	int mid;
-	while(left<=right){
-	mid=(left+right)/2;
-	    if(ar[mid]==target){
-	        flag=1;
-	        break;
-	    }
-	    else if(ar[mid]<target){
-	        left=mid+1;
-	    }
-	    else{
-	        right=mid-1;
-	    }
+	System.out.println("enter the size of array");
+	int n=sc.nextInt();
+	int a[]=new int[n];
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	a[i]=sc.nextInt();
+	for(i=0;i<n-1;i++){
+	 for(j=0;j<(n-1-i);j++){
+	     if(a[j]>a[j+1]){
+	         temp=a[j];
+	         a[j]=a[j+1];
+	         a[j+1]=temp;
+	     }
+	 }
 	}
-	if(flag==1){
-	    System.out.println("found at"+mid);
-	}
-	else{
-	    System.out.println("not found");
-	}
+	 	for(i=0;i<n;i++)
+	 System.out.println(a[i]);
+System.out.println("enter the target");
+int target=sc.nextInt();
+int left=0;
+int right=a.length-1;
+int mid=-1;
+int flag=0;
+while(left<=right){
+    mid=(left+right)/2;
+    if(target==a[mid]){
+        flag=1;
+        break;
+    }
+    else if(target>a[mid]){
+        left=mid+1;
+    }
+    else{
+        right=mid-1;
+    }
+}
+if(flag==1){
+System.out.println("found at"+ mid);
+}
+else{
+System.out.println("do not found");
+}
 	}
 }
